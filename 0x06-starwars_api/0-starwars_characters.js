0b7fdd6c1659e47710415ce0dd9f76f4f2f5875f
Mohammed Elgaily <mbajily@gmail.com>
2024-11-17 18:28:29 +0000

    0x06. Star Wars API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,36 +1,58 @@
 #!/usr/bin/node
 
 const request = require('request');
+
+// Get movie ID from command line arguments
 const movieId = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
 if (!movieId) {
-  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
+  console.error('Please provide a movie ID');
   process.exit(1);
 }
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
+const BASE_URL = 'https://swapi.dev/api';
 
-  const data = JSON.parse(body);
-  const characters = data.characters;
-
-  if (!characters) {
-    console.error('No characters found for the given Movie ID.');
-    return;
-  }
-
-  // Fetch and print characters' names in order
-  characters.forEach((characterUrl) => {
-    request(characterUrl, (err, res, characterBody) => {
-      if (err) {
-        console.error(err);
-        return;
+// Function to make HTTP request as Promise
+function makeRequest(url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(body));
       }
-      console.log(JSON.parse(characterBody).name);
     });
   });
-});
+}
+
+// Function to get character name from URL
+async function getCharacterName(url) {
+  try {
+    const character = await makeRequest(url);
+    return character.name;
+  } catch (error) {
+    console.error(`Error fetching character: ${error}`);
+    return null;
+  }
+}
+
+// Main function to get and print all characters
+async function printMovieCharacters(movieId) {
+  try {
+    // Get movie data
+    const movie = await makeRequest(`${BASE_URL}/films/${movieId}/`);
+    
+    // Get all character names in order
+    const characterPromises = movie.characters.map(url => getCharacterName(url));
+    const names = await Promise.all(characterPromises);
+    
+    // Print names (filtering out any null values from errors)
+    names.filter(name => name !== null).forEach(name => console.log(name));
+  } catch (error) {
+    console.error(`Error: ${error}`);
+    process.exit(1);
+  }
+}
+
+// Execute the main function
+printMovieCharacters(movieId);
